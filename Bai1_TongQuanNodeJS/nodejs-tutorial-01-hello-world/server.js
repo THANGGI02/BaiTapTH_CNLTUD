4c0975edf04ab96277d31e4ae31c2150d5f00e24
Nguyen Dang Toan Thang <thangpower@gmail.com>
2024-07-09 11:21:48 +0700

    Hoàn thành bài thực hành 1

--- Bai1_TongQuanNodeJS/nodejs-tutorial-01-hello-world/server.js
+++ Bai1_TongQuanNodeJS/nodejs-tutorial-01-hello-world/server.js
@@ -1,13 +1,12 @@
 //Nạp module http
 const http = require('http');
 const hostname ='localhost';
-const port = 8017;
+const port = 3000;
 //Tạo server
 const server = http.createServer((req, res)=>{
      //Trạng thái code, 200 là trạng thái thành công
      res.statusCode = 200;
      res.setHeader('Content-Type', 'text/html');
-     res.end('<h1> Hello World </h1>')
-
+     res.end('<h1> Hello World </h1>');
 })
 server.listen(port, hostname);
--- Bai1_TongQuanNodeJS/nodejs-tutorial-02-hello-world/server.js
+++ Bai1_TongQuanNodeJS/nodejs-tutorial-02-hello-world/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const hostname = 'localhost';
-const port = 3000;
+const port = 3001;
 app.get('/', (req, res)=>{
-    res.send('<h1> Hello World </h1>')
+    res.send('<h1> Nguyen Dang Toan Thang </h1>')
 
 });app.listen(port, hostname);
