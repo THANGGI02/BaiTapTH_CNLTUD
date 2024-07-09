77efbce75ad22d2811aae631b59d78271ab08459
Nguyen Dang Toan Thang <thangpower@gmail.com>
2024-07-09 11:25:39 +0700

    Hoàn thành bài thực hành 2

--- Bai2_ModuleTrongNodeJS/server.js
+++ Bai2_ModuleTrongNodeJS/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cat = require('./cat');
 const hostname = 'localhost';
-const port = 8017;
+const port = 3002;
 const app = express();
 app.get('/', (req, res) => {
     let catsArr = ["Huynh Le Thanh Nha", "Ho Ngoc Thien Truong", "Tran Chi Thanh", "Ho Trong Tinh"," Le Van Thanh Long"];
