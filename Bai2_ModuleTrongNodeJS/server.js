18073174f20cd64aae7241e4e8d0b923f7423522
Nguyen Dang Toan Thang <thangpower@gmail.com>
2024-09-06 09:52:23 +0700

    HOÀN THÀNH BÀI THỰC HÀNH SỐ 2

--- Bai2_ModuleTrongNodeJS/server.js
+++ Bai2_ModuleTrongNodeJS/server.js
@@ -3,9 +3,11 @@ const cat = require('./cat');
 const hostname = 'localhost';
 const port = 3002;
 const app = express();
+
 app.get('/', (req, res) => {
     let catsArr = ["Huynh Le Thanh Nha", "Ho Ngoc Thien Truong", "Tran Chi Thanh", "Ho Trong Tinh"," Le Van Thanh Long"];
     let randomCat = cat.getRandomCat(catsArr);
     res.send(`<h1>Cat: <small>${ randomCat }</small></h1><hr>`);
 });
+
 app.listen(port, hostname);
