16470b02b45a24d323b5160f1d9ad490eb12a1d4
Nguyen Dang Toan Thang <thangpower@gmail.com>
2024-09-06 09:53:37 +0700

    HOÀN THÀNH BÀI THỰC HÀNH SỐ 4

--- Bai4_EventModuleTrongNodeJS/server.js
+++ Bai4_EventModuleTrongNodeJS/server.js
@@ -4,11 +4,14 @@ const cat = express();
 const EventEmitter = new events.EventEmitter();
 const hostname = 'localhost';
 const port = 3004;
+
 const ringBell = () => {
     console.log("ring ring ring...");
 }
+
 //Lắng nghe sự kiện
 EventEmitter.on("catRun",ringBell);
+
 //Phát sự kiện
 EventEmitter.emit("catRun");
 cat.listen(port, hostname);
